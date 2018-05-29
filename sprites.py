4d0a3010bcf22de616b1b8971e0ed4225a557a79
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2018-05-29 11:01:12 -0400

    Continued working on collisions, implementing a hit_rect as well as a rect.

--- sprites.py
+++ sprites.py
@@ -8,16 +8,18 @@ def collide_with_walls(sprite, group, dim): #TODO finish the collisions
     hits = pg.sprite.spritecollide(sprite, group, False)
     if dim == 'x':
         for hit in hits:
-            if hit[0].centerx> sprite.rect.centerx:
-                sprite.rect.x = hit[0].x - hit[0].width
-            if hit[0].centerx< sprite.rect.centerx:
-                sprite.rect.x = hit[0].x + hit[0].width
+            if hit[0].rect.centerx> sprite.rect.centerx:
+                sprite.rect.x = hit[0].rect.x - hit[0].rect.width
+            if hit[0].rect.centerx< sprite.rect.centerx:
+                sprite.rect.x = hit[0].rect.x + hit[0].rect.width
+            sprite.vel.x = 0
     if dim == 'y':
         for hit in hits:
-            if hit[0].centery> sprite.rect.centery:
-                sprite.rect.y = hit[0].y - hit[0].height
-            if hit[0].centery< sprite.rect.centery:
-                sprite.rect.y = hit[0].y + hit[0].height
+            if hit[0].rect.centery> sprite.rect.centery:
+                sprite.rect.y = hit[0].rect.y - hit[0].rect.height
+            if hit[0].rect.centery< sprite.rect.centery:
+                sprite.rect.y = hit[0].rect.y + hit[0].rect.height
+            sprite.vel.y = 0
 
 
 class Player(pg.sprite.Sprite):
@@ -28,35 +30,44 @@ class Player(pg.sprite.Sprite):
         self.pos = vec(x,y)
         self.vel = vec(0,0)
         self.image = game.player_img
-        #self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
         self.rot = 0
         self.rot_speed = 0
 
     def get_keys(self):
-        keys = pg.key.get_pressed()
+
         self.vel = vec(0,0)
         self.rot_speed = 0
+        keys = pg.key.get_pressed()
         if keys[pg.K_a]:
             self.rot_speed = ROTATION_SPEED
         if keys[pg.K_d]:
             self.rot_speed = -ROTATION_SPEED
-        self.rot+=self.rot_speed * self.game.dt %360
+
         if keys[pg.K_w]:
             self.vel = vec(PLAYER_SPEED, 0).rotate(-self.rot)
         if keys[pg.K_s]:
-            self.vel = vec(-PLAYER_SPEED, 0).rotate(-self.rot)
+            self.vel = vec(-PLAYER_SPEED / 1.5, 0).rotate(-self.rot)
 
 
 
     def update(self):
         self.get_keys()
-        self.pos += self.vel * self.game.dt
-
+        self.rot += (self.rot_speed * self.game.dt) % 360
         self.image = pg.transform.rotate(self.game.player_img, self.rot)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_rect.centerx = self.pos.x
+        collide_with_walls(self, self.game.walls, 'x')
+        self.hit_rect.centery = self.pos.y
+        collide_with_walls(self, self.game.walls, 'y')
+        self.rect.center = self.hit_rect.center
+
+
 
 
 class Wall(pg.sprite.Sprite):
