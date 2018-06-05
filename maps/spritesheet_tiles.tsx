99ac92e76600c7265639bd82a4c44f26828850aa
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2018-06-05 11:07:17 -0400

    add

---
+++ maps/spritesheet_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet_tiles" tilewidth="64" tileheight="64" tilecount="540" columns="27">
+ <image source="../../../TileGame/img/spritesheet_tiles.png" width="1728" height="1280"/>
+</tileset>
