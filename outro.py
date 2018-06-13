0bedcd990dd55d47249580e26029d07495757d8d
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2018-06-12 22:23:41 -0400

    The outro file is fully documented.

--- outro.py
+++ outro.py
@@ -12,7 +12,10 @@ vec = pg.math.Vector2
 
 
 class Ending:
-    def __init__(self, pokeballs_used, attacks_used, total_kills):
+    '''A class to run the ending scene for the game, will be a screen with scrolling text and pokemon.'''
+
+    def __init__(self, pokeballs_used=3, attacks_used=4, total_kills=5):
+        '''A method to initialize the ending scene with the attributes it needs. Uses Game.load_data for the images.'''
         pg.init()
         self.screen = pg.display.set_mode((OUTRO_WIDTH, OUTRO_HEIGHT))
         pg.display.set_caption(TITLE)
@@ -29,6 +32,7 @@ class Ending:
         self.run_end = True
 
     def load_text(self):
+        '''A method to generate the text surfaces to blit to the screen. '''
         self.outro_lines = ['Congratulations,',
                             'You have trained hard alongside your pokemon,',
                             'You have explored the unexplored,',
@@ -51,31 +55,40 @@ class Ending:
                             'Not to mention you killed HOW MANY POKEMON??',
                             '{}??'.format(self.total_kills),
                             "Pokemon don't grow on trees...",
-                            '',
+                            ' ',
                             "Anyways, it's about time you get going,",
                             "There are more pokemon out there."]
 
-        # make font object for instructions
+        # make font object for outro comments
         self.outro_font = pg.font.Font(font_name, OUTRO_FONT_SIZE)
-        # make list of rendered instructions surfaces, one line per surface
+        # make list of rendered outro comment surfaces, one line per surface
         self.outro_lines_surfaces = [self.outro_font.render(x, True, OUTRO_FONT_COLOUR) for x in self.outro_lines[::-1]]
 
     def update(self):
+        '''A method to move sprites and text down the screen and end the instance when done.'''
         self.sprite_vertical_shift += self.sprite_scroll_rate
         self.text_vertical_shift += self.text_scroll_rate
-        if self.text_vertical_shift > OUTRO_TEXT_LENGTH+OUTRO_HEIGHT:
+        # end the scene
+        if self.text_vertical_shift > OUTRO_TEXT_LENGTH + OUTRO_HEIGHT:
             self.run_end = False
 
     def draw(self):
+        '''A method to draw all the sprites and text to the screen. '''
         self.screen.fill(PALE_BLUE)
-        for num, pokemon in enumerate(self.pokemon_images):
-            vertical_pos = (OUTRO_HEIGHT / (len(self.pokemon_images) - 1) * num + self.sprite_vertical_shift) % (
-                        OUTRO_HEIGHT + 60) - 60
 
+        # draw the pokemon sprites
+        for num, pokemon in enumerate(self.pokemon_images):
+            # set each one's vertical position
+            vertical_pos = (OUTRO_HEIGHT / (len(self.pokemon_images) - 1) * num + self.sprite_vertical_shift) % (
+                    OUTRO_HEIGHT + 60) - 60
+
+            # draw two columns
             self.screen.blit(pokemon, pg.Rect(OUTRO_WIDTH / 8 - 30, vertical_pos, 1, 1))
             self.screen.blit(pokemon, pg.Rect(OUTRO_WIDTH / 8 * 7 - 30, vertical_pos, 1, 1))
 
+        # draw all the text
         for num, line in enumerate(self.outro_lines_surfaces):
+            # work out vertical position, starting off screen.
             vertical_pos = (OUTRO_TEXT_LENGTH / len(
                 self.outro_lines_surfaces) * num + self.text_vertical_shift) - OUTRO_TEXT_LENGTH
 
@@ -84,15 +97,13 @@ class Ending:
         pg.display.flip()
 
     def run(self):
-
+        '''A method to hold a game loop for the ending scene to continually update and draw. '''
         while self.run_end:
+            # game loop
             self.dt = self.clock.tick(FPS) / 1000
             self.update()
             self.draw()
+            # let the use exit early
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.run_end = False
-
-
-e = Ending(4, 5, 6)
-e.run()
